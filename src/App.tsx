ca9b87adbf65e4d7788f4ce121dcd138fc9af8dd
Danil Pobirskiy <danilpobiryan@yandex.ru>
2022-08-19 01:27:07 +0300

    add RTK

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,13 @@ import bodyStyles from "./styles/body.module.css";
 import Header from "./components/header";
 import Sidebar from "./components/sidebar";
 import AppRouter from "./components/router/appRouter";
-import {useAppSelector} from "./hooks/redux";
-import {useDispatch} from "react-redux";
-import {useEffect} from "react";
-import {fetchUsers} from "./store/reducers/ActionCreator";
+import * as React from "react";
 
 function App() {
-  const dispatch = useDispatch()
-  const {users, loading, error} = useAppSelector((state) => state.userReducer);
-
-  // вот с этого момента у меня не рендепится ни один элемент
-  // useEffect(() => {
-  //   dispatch(fetchUsers())
-  // }, [])
-
   return (
     <div className="App">
       <div className={styles.main}>
         <Header />
-        {/*<div></div>*/}
         <div className={cn(bodyStyles.bodyVk, bodyStyles.container)}>
           <div className={bodyStyles.sectionContainer}>
             <Sidebar />
--- src/components/router/appRouter.jsx
+++ src/components/router/appRouter.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import FormikLogin from "../LoginFormik";
 import Main from "../centeer";
-import {BlockPage} from "../pages/Blockpage";
+import { BlockPage } from "../pages/Blockpage";
+import ReduxToolkit from "../ReduxToolkit";
 
 const AppRouter = () => {
   return (
-      <Routes>
-        <Route path="/login" element={<FormikLogin/>} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/news" element={<BlockPage />} />
-        <Route path="*" element={<Main />} />
-      </Routes>
+    <Routes>
+      <Route path="/login" element={<FormikLogin />} />
+      <Route path="/main" element={<Main />} />
+      <Route path="/news" element={<BlockPage />} />
+      <Route path="/reduxToolkit" element={<ReduxToolkit />} />
+      <Route path="*" element={<Main />} />
+    </Routes>
   );
 };
 
